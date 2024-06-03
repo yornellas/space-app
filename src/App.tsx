b7b9e871d19658ba304b89d9433b62489d2d05c0
Yolanda Ornellas <yo_ornellas@hotmail.com>
2024-06-03 15:08:59 -0300

    fix: app name

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Space App - Studying Styled Components</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
