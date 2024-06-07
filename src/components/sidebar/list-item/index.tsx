f8ec97325ff3fe7bb06c51522d2d673d247c0b5c
Yolanda Ornellas <yo_ornellas@hotmail.com>
2024-06-07 08:40:27 -0300

    feat: create list item component

---
+++ src/components/sidebar/list-item/index.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+type ListItemProps = {
+  href: string | undefined
+  text: string
+  activeIcon: string
+  inactiveIcon: string
+  active: boolean
+}
+
+const StyledListItem = styled.li`
+  font-size: 24px;
+
+  a {
+    text-decoration: none;
+    color: #D9D9D9;
+
+    display: flex;
+    align-items: center;
+
+    &.active,
+    &:hover {
+      font-family: 'GandhiSansBold';
+      color: #7B78E5 !important;
+    }
+  }
+`
+
+const ListItem = (
+  { href, activeIcon, inactiveIcon, text, active }: ListItemProps
+) => {
+  return (
+    <StyledListItem>
+      <a href={ href } className={ active ? 'active' : ''}>
+        <img src={ active ? activeIcon : inactiveIcon } alt="" />
+        { text }
+      </a>
+    </StyledListItem>
+  )
+}
+
+export default ListItem
