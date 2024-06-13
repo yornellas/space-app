e86c4716f71b10af3a14583e8821c1c0f8a661be
Yolanda Ornellas <yo_ornellas@hotmail.com>
2024-06-13 14:37:23 -0300

    feat: add align prop to title component

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -1,8 +1,13 @@
 import { styled } from "styled-components";
 
-const Title = styled.h2`
+type TitleProps = {
+  align?: string
+}
+
+const Title = styled.h2<TitleProps>`
   color: #7B78E6;
   font-size: 32px;
+  text-align: ${ props => props.align ? props.align : 'left' };
 `
 
 export default Title
